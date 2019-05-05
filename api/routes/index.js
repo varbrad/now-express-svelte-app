7d67e532261aeb7d71103c9ca151f9589c2e5ddc
Brad Davies <varbrad@users.noreply.github.com>
2019-05-05 18:54:56 +0100

    Fix broken API deployment

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,15 @@
-const readdir = require('recursive-readdir')
 const NotFoundError = require('../errors/NotFoundError')
-// Get all routes in this folder
 
-module.exports = async api => {
-  ;(await readdir(__dirname))
-    .filter(file => file !== __filename) // Remove this file
-    .map(file => require(file)) // Require that file
-    .filter(module => typeof module === 'function') // If it's not a module, then forget it
-    .forEach(module => module(api)) // Call the module
+/**
+ * Routes
+ */
+const routes = [require('./errors'), require('./user')]
 
+module.exports = api => {
+  // Trigger our routes
+  routes.forEach(route => route(api))
+
+  // Fallback route
   api.all('*', req => {
     throw new NotFoundError(
       `The requested API route '${req.originalUrl}' does not exist.`
