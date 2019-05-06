71cd15b3bb7707da6b3c78ce0c368897505fad62
Brad Davies <varbrad@users.noreply.github.com>
2019-05-06 19:53:07 +0100

    Allow the controller to specify it's middlewares

--- src/api/controllers/UserController.js
+++ src/api/controllers/UserController.js
@@ -1,9 +1,13 @@
+const useDatabase = require('../middleware/use/db')
 const User = require('../models/User')
 const safe = require('../utils/safeAsync')
 
 module.exports = {
-  index: safe(async (req, res) => {
-    const users = await User.find()
-    res.api(users)
-  }),
+  index: [
+    useDatabase,
+    safe(async (req, res) => {
+      const users = await User.find()
+      res.api(users)
+    }),
+  ],
 }
--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,6 +1,5 @@
-const useDatabase = require('../middleware/use/db')
 const UserController = require('../controllers/UserController')
 
 module.exports = api => {
-  api.get('/user', useDatabase, UserController.index)
+  api.get('/user', UserController.index)
 }
