c67632c1bfb4cc1e1d9f8e7cc8b4421aa317b482
Brad Davies <varbrad@users.noreply.github.com>
2019-05-06 21:57:24 +0100

    Use the unauth error

--- src/api/middleware/use/auth.js
+++ src/api/middleware/use/auth.js
@@ -1,4 +1,5 @@
 // const mongoose = require('mongoose')
+const UnauthorizedError = require('../../errors/UnauthorizedError')
 
 const getAccessToken = header => {
   const result = /Bearer (.*)/.exec(header)
@@ -10,10 +11,11 @@ module.exports = (req, res, next) => {
   try {
     // Get the authorization header
     const header = req.get('Authorization')
-    if (!header) throw new Error('No Authorization header')
+    if (!header) throw new UnauthorizedError('No Authorization header')
     // Get the access token from the header
     const accessToken = getAccessToken(header)
-    if (!accessToken) throw new Error('Invalid Authorization header')
+    if (!accessToken)
+      throw new UnauthorizedError('Invalid Authorization header')
     // Validate the access token is legitimate
     // TODO: Validate accessToken
     // TODO: Ensure the access token is in date
